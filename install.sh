198c2971089e1e104e366203f5e5d4fdcb269a9d
Israiloff <stardustindark@gmail.com>
2025-07-17 19:10:54 +0500

    fixed: oh my zsh installation commands;

--- install.sh
+++ install.sh
@@ -9,10 +9,6 @@ sudo apt install -y git
 # Install necessary packages
 sudo apt install -y zsh
 
-# Install and configure Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-sed -i -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="half-life"/g' ~/.zshrc
-
 # Setup ZSH syntax highlighting
 sudo apt install -y zsh-syntax-highlighting
 echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> $HOME/.zshrc
@@ -21,4 +17,8 @@ echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> 
 sudo apt install -y zsh-autosuggestions
 echo "source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> $HOME/.zshrc
 
-source $HOME/.zshrc
+# Install and configure Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" -n
+
+# Setting zsh theme to half-life
+sed -i -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="half-life"/g' ~/.zshrc
